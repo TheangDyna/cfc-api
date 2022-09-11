25db78b9c117b2f13cfc5a4f0ed9971419cfff85
Theang Dyna <theangdyna365@gmail.com>
2022-09-11 08:53:17 +0700

    hosting url test

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 //init
-const express = require('express');
-const connectDb = require('./connectDb');
+const express = require("express");
+const connectDb = require("./connectDb");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT||8000;
-const morgan = require('morgan');
+const port = process.env.PORT;
+const morgan = require("morgan");
 //bodyParser
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-require('dotenv').config();
-morgan('tiny')
-
+require("dotenv").config();
+morgan("tiny");
 
 // cors provides Express middleware to enable CORS
+const { baseUrl_client } = require("./utils/constant/baseUrl");
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [baseUrl_client],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
@@ -27,20 +27,19 @@ connectDb();
 //load all route
 
 //users routes
-require('./routes/users.routes')(app);
+require("./routes/users.routes")(app);
 //news routes
-require('./routes/news.routes')(app);
+require("./routes/news.routes")(app);
 //stories routes
-require('./routes/stories.routes')(app);
+require("./routes/stories.routes")(app);
 //students routes
-require('./routes/students.routes')(app);
+require("./routes/students.routes")(app);
 //statuses routes
-require('./routes/events.routes')(app);
+require("./routes/events.routes")(app);
 //communities routes
-require('./routes/communities.routes')(app);
+require("./routes/communities.routes")(app);
 
 //console port
 app.listen(port, () => {
   console.log(`server is start http://localhost:${port}`);
 });
-
---
+++ utils/constant/baseUrl.js
@@ -0,0 +1,19 @@
+const PORT = process.env.PORT;
+// const baseUrl_database =
+//   process.env.NODE_ENV !== "production"
+//     ? `http://localhost:${PORT}`
+//     : process.env.API_URL;
+
+// const baseUrl_client =
+//   process.env.NODE_ENV !== "production"
+//     ? `http://localhost:3000`
+//     : process.env.CLIENT_URL;
+
+const baseUrl_database = process.env.API_URL;
+
+const baseUrl_client = process.env.CLIENT_URL;
+
+module.exports = {
+  baseUrl_database,
+  baseUrl_client,
+};
