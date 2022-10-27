a0daf371df04cbf09676593fb0c81716d4551b59
Theang Dyna <theangdyna365@gmail.com>
2022-10-27 16:46:52 +0700

    update

--- models/events.model.js
+++ models/events.model.js
@@ -1,44 +1,55 @@
-const mongoose = require('mongoose');
-const eventSchema = new mongoose.Schema({
+const mongoose = require("mongoose");
+const eventSchema = new mongoose.Schema(
+  {
     createBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'users',
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
     },
     category: {
-            type: String,
-            enum: ['Hot', 'Event', 'Holiday', 'Scholarship', 'Job', 'Tip'],
-        },
+      type: String,
+      enum: ["Hot", "Event", "Holiday", "Scholarship", "Job", "Tip"],
+    },
     coverName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: String,
+      required: true,
+    },
+    endDate: {
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    date: {
-        type: Date,
-        required: true,
+    location: {
+      type: String,
+      required: true,
     },
     interesting: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'users',
-        },
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
     ],
-    share:  [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'users',
-        },
+    share: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
     ],
-}, { timestamps: true })
+  },
+  { timestamps: true }
+);
 
-const EventsModel = mongoose.model('events', eventSchema);
+const EventsModel = mongoose.model("events", eventSchema);
 
 module.exports = EventsModel;
