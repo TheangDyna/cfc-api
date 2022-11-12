111243ffd19c13cc93a9c760138d077e1ccf26a4
Theang Dyna <theangdyna365@gmail.com>
2022-11-12 08:24:52 +0700

    update

--- controllers/communities.controller.js
+++ controllers/communities.controller.js
@@ -6,7 +6,7 @@ const getCommunities = async (req, res) => {
     try {
 
         //find community id
-        const data = await db.communities.find().populate("createBy.profile");
+        const data = await db.communities.find().populate({ path: "createBy", select: 'firstName' });
         res.status(200).send({
             message: 'Success',
             count: data.length,
