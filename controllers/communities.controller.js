3e160e53142ee924ee7c191f5866ff06a2e5f39c
Theang Dyna <theangdyna365@gmail.com>
2022-11-12 11:30:42 +0700

    update

--- controllers/communities.controller.js
+++ controllers/communities.controller.js
@@ -307,7 +307,7 @@ const getDetailCommunity = async (req, res) => {
     try {
 
         //find event id
-        const findId = await db.communities.findById(communityId).populate({ path: "createBy", select: 'firstName lastName profile' }, { path: "userId", select: 'firstName lastName profile' });
+        const findId = await db.communities.findById(communityId).populate({ path: "createBy", select: 'firstName lastName profile' }, { path: "answer.userId", select: 'firstName lastName profile' });
         if (!findId) return res.status(404).send({ message: 'Not find commnity' });
 
         res.status(200).send({
